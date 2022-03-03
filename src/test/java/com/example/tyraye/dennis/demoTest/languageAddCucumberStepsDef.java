118c8ed9c874d5599f739a16e4d9546fcd7a84f0
Tyraye <tyrasaurasrex@gmail.com>
2022-03-03 16:27:49 +0000

    Creating cucumber tests and testing Jenkins webhook

--- src/test/java/com/example/tyraye/dennis/demoTest/languageAddCucumberStepsDef.java
+++ src/test/java/com/example/tyraye/dennis/demoTest/languageAddCucumberStepsDef.java
@@ -1,33 +1,28 @@
 package com.example.tyraye.dennis.demoTest;
 
-import io.cucumber.core.gherkin.Step;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 
 
-public class menuCucumberStepsDef {
-    String deez = "deez";
-
-    @Given("I have selected a film")
-    public void i_have_selected_a_film() {
-
+public class languageAddCucumberStepsDef {
+    @Given("I have selected to add a language")
+    public void i_have_selected_to_add_a_language() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @When("The film is not in English")
-    public void the_film_is_not_in_english() {
-        deez = "deez";
+    @When("The Language is not in the Database")
+    public void the_language_is_not_in_the_database() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    @Then("I expect to see that it is not available in English")
-    public void i_expect_to_see_that_it_is_not_available_in_english() {
+    @Then("I expect to see that the Language has been added to the database")
+    public void i_expect_to_see_that_the_language_has_been_added_to_the_database() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
 
 }
--- src/test/resources/Cucumber/languageadding.feature
+++ src/test/resources/Cucumber/languageadding.feature
@@ -1,5 +1,5 @@
 Feature: I want to add a new Language into the database
   Scenario: Film receives a new translation that needs to be added to the database
-   Given I have selected a film
-    When The film is not in English
-    Then I expect to see that it is not available in English
+   Given I have selected to add a language
+    When The Language is not in the Database
+    Then I expect to see that the Language has been added to the database
